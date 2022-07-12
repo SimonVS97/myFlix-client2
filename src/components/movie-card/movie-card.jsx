265ff7c6df26d676c6386e70cf0f07a68d1f71a9
SimonVS97 <104713327+SimonVS97@users.noreply.github.com>
2022-07-12 19:34:23 +0200

    added PropTypes

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class MovieCard extends React.Component {
   render() {
     const movie = this.props.movieData;
     const onMovieClick = this.props.onMovieClick;
-    return <div onClick={() => { onMovieClick(movie); }} className='movie-card'> {movie.Title} </div>
+
+    return (
+      <div onClick={() => { onMovieClick(movie); }} className='movie-card'> {movie.Title} </div>
+    );
   }
 }
+
+MovieCard.PropTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
